b39f2a93109bb6be4b10a2ce0e06a2a89c88c422
maryogaJUG <maryoga@hotmail.com>
2023-07-24 23:35:05 -0600

    Update Hook useRef

--- 05-hook-app/src/04-useRef/FocusScreen.jsx
+++ 05-hook-app/src/04-useRef/FocusScreen.jsx
@@ -1,14 +1,31 @@
-
+import { useRef } from "react"
 
 export const FocusScreen = () => {
+
+    const inputRef = useRef();
+
+    const onClick = () => {
+        // document.querySelector('input').select();
+        // console.log(inputRef);
+        inputRef.current.select();
+    }
+
   return (
     <>
         <h1>Focus Screen</h1>
         <hr />
-        <input 
+        <input
+            ref = { inputRef }
             type="text"
             placeholder="Enter your name"
+            className="form-control"
         />
+        <button
+            className="btn btn-primary mt-2"
+            onClick={ onClick }
+        >
+            Set focus
+        </button>
     </>
   )
 }
