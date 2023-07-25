08fb7a9f27c197f7544ea50be54325fe8056edcc
maryogaJUG <maryoga@hotmail.com>
2023-07-24 18:30:28 -0600

    Adding Hooks - useRef

---
+++ 05-hook-app/src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,14 @@
+
+
+export const FocusScreen = () => {
+  return (
+    <>
+        <h1>Focus Screen</h1>
+        <hr />
+        <input 
+            type="text"
+            placeholder="Enter your name"
+        />
+    </>
+  )
+}
